2f6dcee551d3e57b8539d34f782477e309646e73
Jon Chernus <jmc108@pitt.edu>
2026-01-22 08:56:31 -0500

    Updated ex4

--- exercises/4_amounts_and_tables/setup.R
+++ exercises/4_amounts_and_tables/setup.R
@@ -1,6 +1,29 @@
 library(tidyverse)
 library(vroom)
 
+
+
+##########
+meta_url <- "https://raw.githubusercontent.com/sheffield-bioinformatics-core/wrangling-genomics/gh-pages/files/1000_genomes_meta.tsv"
+d <- readr::read_tsv(meta_url, show_col_types = FALSE) |>
+  janitor::clean_names()
+# Keep just what we need (names in this file are stable, but we’ll be explicit)
+d <- readr::read_tsv(meta_url, show_col_types = FALSE) |>
+  janitor::clean_names() |>
+  transmute(
+    sample = sample_name,
+    sex = factor(sex),
+    pop = population_code,
+    pop_name = population_name,
+    superpop = superpopulation_code,        # <-- FIX
+    superpop_name = superpopulation_name
+  )
+head(d)
+write.csv(x=d, file="/Users/jonathanchernus/Documents/Teaching/hugen2073-textbook/exercises/4_amounts_and_tables/d.csv", quote = FALSE)
+
+##########
+
+
 clinvar_url <- "https://ftp.ncbi.nlm.nih.gov/pub/clinvar/tab_delimited/variant_summary.txt.gz"
 
 cv <- vroom::vroom(
