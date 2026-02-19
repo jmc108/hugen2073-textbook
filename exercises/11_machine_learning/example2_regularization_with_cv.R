38fac685acecdeb58e40edd72f6ac71ee1f7d55b
Quarto GHA Workflow Runner <quarto-github-actions-publish@example.com>
2026-02-19 23:56:40 +0000

    Built site for gh-pages

--- exercises/11_machine_learning/example2_regularization_with_cv.R
+++ exercises/11_machine_learning/example2_regularization_with_cv.R
@@ -37,6 +37,21 @@ gene_ids <- featureNames(Golub_Merge)
 n <- nrow(X); p <- ncol(X)
 cat("n =", n, "p =", p, "\n")
 
+save.image("/Users/jonathanchernus/Documents/Teaching/hugen2073-textbook/exercises/11_machine_learning/example2.RData")
+
+
+detach("package:golubEsets", unload = TRUE)
+detach("package:Biobase", unload = TRUE)
+detach("package:BiocGenerics", unload = TRUE)
+
+#library(golubEsets)
+library(glmnet)
+library(pROC)
+
+load("/Users/jonathanchernus/Documents/Teaching/hugen2073-textbook/exercises/11_machine_learning/example2.RData")
+
+
+
 # 0.  Train/Test split
 set.seed(1)
 train_frac <- 0.7
